67bad70c439360b80e32f42ae0fbfb531a629df2
student-dv <dvbeast465@gmail.com>
2025-02-02 21:46:05 +0530

    update api

--- src/services/QuizService.js
+++ src/services/QuizService.js
@@ -2,21 +2,30 @@ import axios from "axios";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? `https://cors-anywhere.herokuapp.com/https://api.jsonserve.com/Uw5CrX`
+    ? `https://api.allorigins.win/get?url=${encodeURIComponent(
+        "https://api.jsonserve.com/Uw5CrX"
+      )}`
     : "/api/Uw5CrX";
 
 export const fetchQuizData = async () => {
   try {
     const response = await axios.get(API_URL, {
       headers: {
-        "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
       },
     });
-
     return response.data;
   } catch (error) {
     console.error("Error fetching quiz data:", error);
+    if (error.response) {
+      console.error("Server response data:", error.response.data);
+      console.error("Server response status:", error.response.status);
+      console.error("Server response headers:", error.response.headers);
+    } else if (error.request) {
+      console.error("No response received:", error.request);
+    } else {
+      console.error("Error setting up the request:", error.message);
+    }
     throw error;
   }
 };
