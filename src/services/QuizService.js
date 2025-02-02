b78c1580573e03e0bd6324cc2807ca0c2206cd6a
student-dv <dvbeast465@gmail.com>
2025-02-02 20:59:48 +0530

    add service

--- src/services/QuizService.js
+++ src/services/QuizService.js
@@ -2,18 +2,12 @@ import axios from "axios";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? `https://cors-anywhere.herokuapp.com/https://api.jsonserve.com/Uw5CrX`
-    : "/api/Uw5CrX";
+    ? "https://your-deployed-proxy.com/api/quiz"
+    : "http://localhost:5173/api/quiz";
 
 export const fetchQuizData = async () => {
   try {
-    const response = await axios.get(API_URL, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      },
-    });
-
+    const response = await axios.get(API_URL);
     return response.data;
   } catch (error) {
     console.error("Error fetching quiz data:", error);
